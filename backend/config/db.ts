82358397319fc93eea48b26860001d57ba4918b5
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2023-06-07 16:43:04 -0300

    fix:

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose'
 
 const connectDB = async () => {
+	if (!process.env.MONGO_URI) {
+		throw new Error(
+			'Please define the MONGO_URI environment variable inside .env.local'
+		)
+	}
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI || '')
-
 		console.log(`MongoDB Connected: ${conn.connection.host}`)
 	} catch (error: any) {
 		console.error(`Error: ${error.message}`)
